c92f3579b8234b8fb89a51c0dd0fdb4603f6d8aa
Akbar Eka Putra <akbarekaputra01@gmail.com>
2026-04-11 20:47:32 +0700

    feat: put back import useState

--- aldes-burger-frontend/src/App.jsx
+++ aldes-burger-frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import { CartProvider } from './context/CartContext'
